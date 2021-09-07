63978e640c22432e62726721247b7f5e016276c9
Tirta Mulya <tmulya@gmail.com>
2021-09-07 23:11:23 +0700

    fix

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,30 +1,90 @@
 <template>
     <div>
-        <section id="hero2" class="d-flex align-items-center">
-            <div class="container">
-                <h1>Contact</h1>
-            </div>
+        <section id="hero2" class="section-bg">
         </section>
-        <section id="about" class="about section-bg">
-            <div class="container" data-aos="fade-up">
-                <h3 class="headline-primary">Mamuju Prima Makmur</h3>
-                <p>PT Mamuju Prima Makmur (MPM) didirikan pada tanggal 4 September 2019 dengan Visi menjadi perusahaan terkemuka yang kompeten dan berdaya saing unggul untuk kebutuhan berkelanjutan pembangunan dalam bidang Jasa Konstruksi, MEP (Mechanical, Electrical, Plumbing) dan juga bidang Penyimpanan Minyak Dan Gas Bumi (MIGAS) di Indonesia.</p>
-                <p>PT Mamuju Prima Makmur berkomitmen untuk memiliki tata nilai yang tepat dalam penerapan good corporate governance dan terus berinovasi mewujudkan perusahaan yang unggul dan kompeten pada sektor penyimpanan minyak dan gas bumi serta pendukungnya.</p>
-                <p>Kegiatan bisnis kami termasuk dalam bidang instalasi mekanikal, konstruksi khusus, konstruksi bangunan sipil, instalasi/pemasangan mesin dan peralatan industri, konstruksi Gedung industri dan Gedung, perdagangan besar bahan bakar padat, cair dan gas dan produk yang berkaitan dengan itu, angkutan bermotor untuk barang khusus, angkutan laut dalam negeri untuk barang khusus termasuk pergudangan dan penyimpanannya.</p>
-                <h3 class="text-center headline-primary">Visi</h3>
-                <h5 class="text-center">&#8220;Menjadi perusahaan terkemuka yang kompeten dan berdaya saing untuk kebutuhan berkelanjutan pembangunan Indonesia&#8220;</h5>
-                <h3 class="text-center headline-primary">Misi</h3>
-                <h5 class="text-center">&#8220;Melaksanakan usaha yang mendukung sektor Minya dan Gas Bumi dengan pengoperasian yang terkendali sesuai standar, memiliki nilai lebih dan selaras dengan kemajuan era globalisasi&#8220;</h5>
-                <h5 class="text-center">&#8220;Mengembangkan budaya profesionalisme, kompetensi serta kesejahteraan pegawai.&#8220;</h5>
-                <h3 class="text-center headline-primary">Strategi</h3>
-                <h5 class="text-center">&#8220;Fokus pada kegiatan oil Konstruksi bangunan sipil minya dan gas bumi, penyimpanan minyak dan gas bumi dan pergudangan dan penyimpanan lainnya.&#8220;</h5>
-                <h5 class="text-center">&#8220;Kegiatan usaha dikelola dengan mengoptimalkan dan mengembangkan serta memanfaatkan sumber daya perusahaan secara sehat dengan tetap memelihara .&#8220;</h5>
+        <!-- ======= Contact Section ======= -->
+        <section id="contact" class="contact">
+      <div class="container">
+        <div class="section-title">
+          <h2>Contact</h2>
+          <h3><span>Contact Us</span></h3>
+        </div>
+        <div class="row">
+            <div class="col-lg-6 ">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="info-box mb-4">
+                            <i class="bx bx-map"></i>
+                            <h3>PT. Groot Perkasa</h3>
+                            <p>Jl. Lkr. Luar Barat No.1 Ciputra International (Tokopedia Care Tower) Lt.1 No. 02-06 Jakarta</p>
+
+                            <h4>Phone</h4>
+                            <p>+62 888888</p>
+                            <h4>Email</h4>
+                            <p></p>
+                        </div>
+                    </div>
+                </div>
             </div>
+            <div class="col-lg-6">
+                <form >
+                    <div class="form-group">
+                        <input type="text" v-model="contact.name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                        <div class="validate"></div> 
+                    </div>
+                    <div class="form-group">
+                        <input type="email" class="form-control" v-model="contact.email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                        <div class="validate"></div>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" v-model="contact.phone" id="phone" placeholder="Phone +62" data-rule="minlen:4" data-msg="Please enter at least 8 chars of phone" />
+                        <div class="validate"></div>
+                    </div>
+                    <div class="form-group">
+                        <textarea class="form-control" v-model="contact.message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
+                        <div class="validate"></div>
+                    </div>
+                    <div class="text-center"><button type="button" class="btn-get-started" @click="send">Send Message</button></div>
+                </form>
+            </div>
+
+        </div>
+
+      </div>
         </section>
+        <!-- End Contact Section -->
     </div> 
 </template>
 
 <script>
+import axios from 'axios'
+import { domainApi } from '@/config'
 export default {
+  data () {
+    return {
+      contact: {
+        name: null,
+        email: null,
+        phone: null,
+        message: null
+      }
+    }
+  },
+  methods: {
+    async send () {
+      try {
+        const response = await axios.post(domainApi + '/contacts', this.contact)
+        console.log(response)
+        this.contact = {
+          name: null,
+          email: null,
+          phone: null,
+          message: null
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
 }
 </script>
